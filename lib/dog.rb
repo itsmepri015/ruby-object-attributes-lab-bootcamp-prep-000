d37e07282b8107d3b7c382a014e198302b069628
naizal Edy <pritiadhikari@gmail.com>
2018-10-18 03:41:49 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,8 +1,5 @@
 class Dog 
 
-def initialize(name)
- @name = name
-end
 
 def name
  @name
@@ -12,9 +9,6 @@ def name=(new_name)
   @name =new_name
 end
 
-def initialize(breed)
- @breed = breed
-end
 
 def breed
  @breed
